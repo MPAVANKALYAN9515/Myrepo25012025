c0d603e56820a48dd3a6268a2eaa89e4b3693604
MPAVANKALYAN9515 <pavankalyan1691@gmail.com>
2025-01-26 01:16:44 +0530

    Renamed

--- ByteStreams_file_class.java
+++ ByteStreams_file_class.java
@@ -2,36 +2,29 @@ package com.file.streamstypes;
 
 import java.io.*;
 
-public class ByteStreams_file_class {
+public class  ByteStreams_file_class {
 
 	public static void main(String[] args) throws IOException {
 		File f = new File("C:\\Users\\pavan\\NNW\\king.txt");
 		System.out.println(f.exists());
 		f.createNewFile();
 		System.out.println(f.exists());
+		System.out.println("==================================================================");
 		
 		File f1 = new File("C:\\Users\\pavan\\NNW" , "K15678.txt");
 		System.out.println(f1.exists());
 		f1.mkdir();
 		System.out.println(f1.exists());
+
+		System.out.println("==================================================================");
 		
 		File f2 = new File(f1,"mounica.txt");
 		System.out.println(f2.exists());
 		f2.createNewFile();
 		System.out.println(f2.exists());
-		
-		
-//		String Str = "https://tcsglobal.udemy.com/";
-//		FileInputStream fIN = new FileInputStream("C:\\\\Users\\\\pavan\\\\NNW\\\\Summer.txt");
-//		int count = fIN.available();
-//		byte[] b= new byte[count];
-//		String S = b.toString();
-//		System.out.println(count);
-//		String[] S1 = fIN;
-//		FileOutputStream Foutv = new FileOutputStream("C:\\Users\\pavan\\NNW\\Summer.txt", false);
-//		byte b[] = Str.getBytes();
-//		Foutv.write(b);
 		System.out.println("Executed");
+
+		System.out.println("==================================================================");
 		
 	}
 }
