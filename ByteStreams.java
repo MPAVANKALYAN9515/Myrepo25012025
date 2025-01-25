19970cd8503c675870b1653edd8d96835ceb6cd4
Mahadasa Chandu Siva Sankara  Pavan Kalyan <119737236+MPAVANKALYAN9515@users.noreply.github.com>
2025-01-25 21:40:12 +0530

    Update02 ByteStreams.java

--- ByteStreams.java
+++ ByteStreams.java
@@ -9,17 +9,23 @@ public class ByteStreams {
 		System.out.println(f.exists());
 		f.createNewFile();
 		System.out.println(f.exists());
+		System.out.println("==================================================================");
 		
 		File f1 = new File("C:\\Users\\pavan\\NNW" , "K15678.txt");
 		System.out.println(f1.exists());
 		f1.mkdir();
 		System.out.println(f1.exists());
+
+		System.out.println("==================================================================");
 		
 		File f2 = new File(f1,"mounica.txt");
 		System.out.println(f2.exists());
 		f2.createNewFile();
 		System.out.println(f2.exists());
 		System.out.println("Executed");
+
+
+		System.out.println("==================================================================");
 		
 	}
 }
